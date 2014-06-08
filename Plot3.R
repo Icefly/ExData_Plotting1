5d67a413c1e8202254c56af627fc4e610a67df0d
Icefly <Icefly@users.noreply.github.com>
2014-06-08 07:26:37 -0400

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,8 +1,10 @@
 a<-read.table("./Data & Analytics/Explanatory Data Analysis Course/Project 1/household_power_consumption.txt", 
+a<-read.table("./Data & Analytics/Explanatory Data Analysis Course/Project 1/household_power_consumption.txt", 
               sep = ";", header = TRUE, na.strings = "?")
 a$Date <-as.Date(as.character(a$Date), format="%d/%m/%Y")
 a<-a[a[,"Date"]>="2007-02-01",]
 a<-a[a[,"Date"]<="2007-02-02",]
+a$NewDate<-strptime(paste(a$Date,a$Time),"%Y-%m-%d %H:%M:%S")
 
 png(file = "plot3.png", width = 480, height = 480, units = "px")
 plot(a$NewDate, a$Sub_metering_1, type = "l", 
