69e2d670442db209788918eefe75d1f2605196ca
Icefly <Icefly@users.noreply.github.com>
2014-06-08 07:26:04 -0400

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -4,9 +4,9 @@ a$Date <-as.Date(as.character(a$Date), format="%d/%m/%Y")
 a<-a[a[,"Date"]>="2007-02-01",]
 a<-a[a[,"Date"]<="2007-02-02",]
 y_title <- "Global Active Power (kilowatts)"
+a$NewDate<-strptime(paste(a$Date,a$Time),"%Y-%m-%d %H:%M:%S")
 
 png(file = "plot2.png", width = 480, height = 480, units = "px")
-a$NewDate<-strptime(paste(a$Date,a$Time),"%Y-%m-%d %H:%M:%S")
 plot(a$NewDate,a$Global_active_power, type = "l", 
      ylab = y_title, xlab = "")
 dev.off()
