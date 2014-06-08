d69b3c25adee12b9192a0c488601394d9d0da17c
Icefly <Icefly@users.noreply.github.com>
2014-06-08 07:24:07 -0400

    Plot3.R
    This is the R code to create the plot 3.

---
+++ Plot3.R
@@ -0,0 +1,13 @@
+a<-read.table("./Data & Analytics/Explanatory Data Analysis Course/Project 1/household_power_consumption.txt", 
+              sep = ";", header = TRUE, na.strings = "?")
+a$Date <-as.Date(as.character(a$Date), format="%d/%m/%Y")
+a<-a[a[,"Date"]>="2007-02-01",]
+a<-a[a[,"Date"]<="2007-02-02",]
+
+png(file = "plot3.png", width = 480, height = 480, units = "px")
+plot(a$NewDate, a$Sub_metering_1, type = "l", 
+     ylab = "Energy sub metering", xlab = "")
+lines(a$NewDate, a$Sub_metering_2, col = "red")
+lines(a$NewDate, a$Sub_metering_3, col = "blue")
+legend("topright", pch = "_", col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
