3f5d5d3e38c226c7afa71a4ed27aece9dd3a4d2a
Icefly <Icefly@users.noreply.github.com>
2014-06-08 07:23:27 -0400

    Plot2.R
    This is the R Code to create the plot 2.

---
+++ Plot2.R
@@ -0,0 +1,12 @@
+a<-read.table("./Data & Analytics/Explanatory Data Analysis Course/Project 1/household_power_consumption.txt", 
+              sep = ";", header = TRUE, na.strings = "?")
+a$Date <-as.Date(as.character(a$Date), format="%d/%m/%Y")
+a<-a[a[,"Date"]>="2007-02-01",]
+a<-a[a[,"Date"]<="2007-02-02",]
+y_title <- "Global Active Power (kilowatts)"
+
+png(file = "plot2.png", width = 480, height = 480, units = "px")
+a$NewDate<-strptime(paste(a$Date,a$Time),"%Y-%m-%d %H:%M:%S")
+plot(a$NewDate,a$Global_active_power, type = "l", 
+     ylab = y_title, xlab = "")
+dev.off()
